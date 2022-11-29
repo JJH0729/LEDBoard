e0d2c64c33228a44c3a617123c6dfc0ba46ae2de
장종환 <98254580+JJH0729@users.noreply.github.com>
2022-11-29 23:53:07 +0900

    프로젝트 추가

---
+++ LEDBoard/LEDBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LEDBoard
+//
+//  Created by  장종환 on 2022/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
